79f22aed9995eb2e6ca1d0fd7a69667269d131ed
jtakaishi <junji.takaishi@gmail.com>
2016-04-14 13:25:49 +0000

    add gravator

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -12,11 +12,11 @@ class OwnershipsController < ApplicationController
     if @item.new_record?
       begin
         # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
-        response = {}
+        response = Amazon::Ecs.item_lookup(params[:asin],{ :response_group => 'Medium',:country => 'jp'})
       rescue Amazon::RequestError => e
         return render :js => "alert('#{e.message}')"
       end
-
+      
       amazon_item       = response.items.first
       @item.title        = amazon_item.get('ItemAttributes/Title')
       @item.small_image  = amazon_item.get("SmallImage/URL")
@@ -30,8 +30,6 @@ class OwnershipsController < ApplicationController
     # TODO ユーザにwant or haveを設定する
     # params[:type]の値にHaveボタンが押された時には「Have」,
     # Wantボタンが押された時には「Want」が設定されています。
-    
-    
     
     if params[:type] == "Want"
       # itemをwantする
